007859296dc8581b41db1c6c3389d64ea37cfd15
David Hawking <dahawkin@microsoft.com>
2018-01-02 00:42:11 +0000

    Addressing two issues found by MacOS CLANG compiler

--- src/qbashq-lib/query_shortening.h
+++ src/qbashq-lib/query_shortening.h
@@ -2,10 +2,12 @@
 // Licensed under the MIT license.
 
 
+#if !defined(__APPLE__)
+// on MacOS qsort_r is defined in stdlib.h.  Not sure why it isn't on Windows
 void qsort_r(void *base, size_t nmemb, size_t size,
                   int (*compar)(const void *, const void *, void *),
                   void *arg);
-
+#endif
 
 void create_candidate_generation_query(query_processing_environment_t *qoenv,
 				       book_keeping_for_one_query_t *qex);
